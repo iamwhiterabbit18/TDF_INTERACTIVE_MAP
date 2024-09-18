295cc377261d9e90ff7dd0e4eb72ee687487806a
Gene <mangueragene@gmail.com>
2024-09-19 00:14:04 +0800

    ADD:FIRSTMAP

---
+++ src/Components/Map.js
@@ -0,0 +1,20 @@
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
+import * as THREE from 'three';
+
+export default class Map {
+    constructor(scene){
+        this.map = new GLTFLoader();
+        this.map.setPath('map/');
+        this.map.load('map.gltf', (gltfScene) => {
+            const mesh = gltfScene.scene;
+            mesh.rotation.x = 0.5 * Math.PI;
+            scene.add(mesh);
+
+            mesh.traverse((object) => {
+                if (object.isMesh) {
+                  object.material.extensions = extensions;
+                }
+              });
+        });
+    }
+}
